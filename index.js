719789d289bc074870ad8fb9a70a7e9826fb65d0
Snowkster <59650977+Snowkster@users.noreply.github.com>
2022-04-07 16:28:48 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,75 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const DisTube = require('distube');
+const distube = new DisTube(client, {
+    serchSongs: false,
+    emitNewSongOnly: true
+});
+const {
+    token
+} = require("./info.json");
+const prefix = '?';
+
+client.on("ready", () => {
+    console.log(`${client.user.tag} has logged in.`);
+});
+
+client.on("messageCreate", async (message) => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift();
+    const command = args.shift().toLowerCase()
+    const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command))
+    if (!cmd) return
+    if (cmd.inVoiceChannel && !message.member.voice.channel) {
+        return message.channel.send(`${client.emotes.error} | You must be in a voice channel!`)
+    }
+    try {
+        cmd.run(client, message, args)
+    } catch (e) {
+        console.error(e)
+        message.channel.send(`${client.emotes.error} | Error: \`${e}\``)
+    }
+
+
+const status = queue =>
+    `Volume: \`${queue.volume}%\` | Filter: \`${queue.filters.join(', ') || 'Off'}\` | Loop: \`${
+    queue.repeatMode ? (queue.repeatMode === 2 ? 'All Queue' : 'This Song') : 'Off'
+  }\` | Autoplay: \`${queue.autoplay ? 'On' : 'Off'}\``
+client.distube
+    .on('playSong', (queue, song) =>
+        queue.textChannel.send(
+            `${client.emotes.play} | Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user.tag}\n${status(queue)}`
+        )
+    )
+    .on('addSong', (queue, song) =>
+        queue.textChannel.send(
+            `${client.emotes.success} | Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user.tag}`
+        )
+    )
+    .on('addList', (queue, playlist) =>
+        queue.textChannel.send(
+            `${client.emotes.success} | Added \`${playlist.name}\` playlist (${
+        playlist.songs.length
+      } songs) to queue\n${status(queue)}`
+        )
+    )
+    .on('error', (channel, e) => {
+        channel.send(`${client.emotes.error} | An error encountered: ${e.toString().slice(0, 1974)}`)
+        console.error(e)
+    })
+    .on('empty', channel => channel.send('Voice channel is empty! Leaving the channel...'))
+    .on('searchNoResult', (message, query) =>
+        message.channel.send(`${client.emotes.error} | No result found for \`${query}\`!`)
+    )
+    .on('finish', queue => queue.textChannel.send('Finished!'))
+
+    if (command == "play") {
+        if(message.member.voice.channel) return message.channel.send('You are not in a voice channel');
+        if(!args[0]) return message.channel.send('You mus t state something to play!');
+        distube.play(message, args.join(" "));
+    }
+});
+
+client.login(token);
